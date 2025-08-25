a79d4a37550b5c4e699fa4ac9479bcac3748adac
tahmidarefin <tahmidarefin@gmail.com>
2025-08-26 00:17:05 +0600

    create a reusable Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <>
-      <div className="h-screen w-full flex items-center justify-center bg-gray-900">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-emerald-800 text-white px-4 py-2 rounded hover:bg-emerald-600 cursor-pointer"
-        >
-          Count is {count}
-        </button>
+      <Header />
+      <hr />
+      <div>
+        Main Content
       </div>
     </>
   );
