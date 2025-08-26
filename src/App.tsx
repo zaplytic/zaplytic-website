5e228c3596fef90203570c212e5000c4ba7b71fc
tahmidarefin <tahmidarefin@gmail.com>
2025-08-27 02:51:53 +0600

    refactor: create a reusable Header component
    resolves #3
    
    - `/src/components/Header.tsx`: used regular function as component
    - `/src/App.tsx`: used absoulte path as variable path for importing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import Header from "@/components/Header"
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
+      <Header />
       <div className="h-screen w-full flex items-center justify-center bg-gray-900">
         <button
           onClick={() => setCount((count) => count + 1)}
