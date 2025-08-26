d2e3247d0f7ab07f54e8d6a158493dc49280da50
tahmidarefin <tahmidarefin@gmail.com>
2025-08-27 01:59:07 +0600

    refactor: changed to dafault absolute path in config files

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <>
+            <h2>Follow Us</h2>
+            <div>
+                <a href="https://github.com/zaplytic" target="_blank">Github</a> &nbsp;
+                <a href="#" target="_blank">Twitter</a>
+            </div>
+            <div>&#169; Zaplytic 2025<sup>&#174;</sup>. All Rights Reserved.</div>
+        </>
+    );
+}
