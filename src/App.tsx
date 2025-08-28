c83bf915353ebbf93172eedceecc382a21a34bb4
tahmidarefin <tahmidarefin@gmail.com>
2025-08-29 01:23:32 +0600

    feature: set up client-side routing with react-router-dom
    resolves #6
    
    - `yarn.lock`: The react-router-dom library is added
    - `package-lock.json`: The react-router-dom library is added
    - `package.json`: The react-router-dom library is added
    - `src/App.tsx`: The main App.tsx is wrapped for routing in a BrowserRouter.
    - `src/pages/Home.tsx`: Home page is created to configure a basic route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
@@ -8,16 +9,21 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
-      <Header />
-      <div className="h-screen w-full flex items-center justify-center bg-gray-900">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-emerald-800 text-white px-4 py-2 rounded hover:bg-emerald-600 cursor-pointer"
-        >
-          Count is {count}
-        </button>
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+        <div className="h-screen w-full flex items-center justify-center bg-gray-900">
+          <button
+            onClick={() => setCount((count) => count + 1)}
+            className="bg-emerald-800 text-white px-4 py-2 rounded hover:bg-emerald-600 cursor-pointer"
+          >
+            Count is {count}
+          </button>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
