c5045e343d42d48a266a36dfe54b82c6bd7fcb5e
Jahid Hasan <jahidhasanimonn@gmail.com>
2025-09-01 01:29:22 +0600

    chore: add husky pre-commit hook
    - add packageManager to package.json
    - add prettier.config.js for prettier
    - delete package-lock.json

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  bracketLine: true,
+  arrowParens: "always",
+  singleAttributePerLine: false
+};
+
+export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
-
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
