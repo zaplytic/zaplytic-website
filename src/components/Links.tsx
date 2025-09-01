636557416594fe42568f6171b34e1859de1c722b
tahmidarefin <tahmidarefin@gmail.com>
2025-09-01 19:42:21 +0600

    refactor: fixed all indentaions with prettier command from yarn

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,12 +1,16 @@
 export default function Links() {
-    return (
-        <>
-            <div>
-                <a href="https://github.com/zaplytic" target="_blank">Zaplytic on Github</a>
-            </div>
-            <div>
-                <a href="#" target="_blank">Zaplytic on Twitter</a>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <a href="https://github.com/zaplytic" target="_blank">
+          Zaplytic on Github
+        </a>
+      </div>
+      <div>
+        <a href="#" target="_blank">
+          Zaplytic on Twitter
+        </a>
+      </div>
+    </>
+  );
 }
